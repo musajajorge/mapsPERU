03f0c3631127f14ec050b18c8c29511c8a2ae2f2
Jorge L. C. Musaja <73267844+musajajorge@users.noreply.github.com>
2021-10-30 00:16:26 -0500

    Version 0.1.4

---
+++ R/centroids_DEP.R
@@ -0,0 +1,10 @@
+#' Departmental centroids of Peru
+#' @format dataframe
+#' \describe{
+#'   \item{long}{dbl Longitude of the departmental centroid}
+#'   \item{lat}{dbl Latitude of the departmental centroid}
+#'   \item{COD_DEPARTAMENTO}{chr Department Code}
+#'   \item{DEPARTAMENTO}{chr Department name}
+#'   \item{geometry}{GEOMETRY Geometric object}
+#' }
+"centroids_DEP"
