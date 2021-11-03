ae33729532a9d87559e436e117f1e6e6def757af
Jorge L. C. Musaja <73267844+musajajorge@users.noreply.github.com>
2021-11-02 19:13:32 -0500

    Version 1.0.0

---
+++ R/map_DIST.R
@@ -0,0 +1,12 @@
+#' Geographic information of the districts of Peru
+#' @format dataframe
+#' \describe{
+#'   \item{COD_DISTRITO}{chr District Code}
+#'   \item{DEPARTAMENTO}{chr Department name}
+#'   \item{PROVINCIA}{chr Province name}
+#'   \item{DISTRITO}{chr District name}
+#'   \item{coords_x}{dbl Latitude of the centroid of the district}
+#'   \item{coords_y}{dbl Longitude of the centroid of the district}
+#'   \item{geometry}{MULTIPOLYGON Geometric object}
+#' }
+"map_DIST"
