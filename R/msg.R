4b0aa6b91eca7fee3370d327301ede551cd9eaf9
Jorge L. C. Musaja <73267844+musajajorge@users.noreply.github.com>
2021-11-13 18:59:43 -0500

    Delete R directory

--- R/msg.R
+++
@@ -1,9 +0,0 @@
-.onAttach <-
-  function(libname, pkgname)
-  {
-    #packageStartupMessage("")
-    #packageStartupMessage("                           =======================================")
-    packageStartupMessage("For examples, please visit https://github.com/musajajorge/mapsPERU")
-    #packageStartupMessage("                           =======================================")
-    packageStartupMessage("")
-  }
